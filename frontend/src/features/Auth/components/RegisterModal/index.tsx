ace2418b751e3ee52508011c1bd795e194b0cdbc
Nastya <the.n_kocherovaaa@bk.ru>
2025-06-06 14:27:09 +0300

    added main visual for modal

--- frontend/src/features/Auth/components/RegisterModal/index.tsx
+++ frontend/src/features/Auth/components/RegisterModal/index.tsx
@@ -1,46 +1,66 @@
 import styles from './RegisterModal.module.scss';
 import iconArray from '@icon/icon-array.svg';
-import { useState, useRef, useEffect, ChangeEvent } from 'react';
+import { useState, useRef, useEffect, ChangeEvent, KeyboardEvent } from 'react';
 
 const CODE_LENGTH = 6;
 
 interface RegisterModalProps {
-  onClose: () => void; 
+  onClose: () => void;
 }
 
-
-function RegisterModal({ onClose } : RegisterModalProps) {
-  const [codes, setCodes] = useState<string[]>(Array(CODE_LENGTH).fill(0));
+function RegisterModal({ onClose }: RegisterModalProps) {
+  const [codes, setCodes] = useState<string[]>(Array(CODE_LENGTH).fill(''));
 
   const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>, index: number) => {
+    if (e.key === 'Backspace') {
+      if (codes[index] === '' && index > 0) {
+        setCodes((prevCodes) => {
+          const newCodes = [...prevCodes];
+          newCodes[index - 1] = ''; 
+          return newCodes;
+        });
+        inputRefs.current[index - 1]?.focus(); 
+      } else if (codes[index] !== '') {
+        setCodes((prevCodes) => {
+          const newCodes = [...prevCodes];
+          newCodes[index] = '';
+          return newCodes;
+        });
+      }
+    }
+  };
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>, index: number) => {
     const value = e.target.value;
     const enteredChar = value.slice(0, 1);
 
-    if (enteredChar && /^\d$/.test(enteredChar)){
-      setCodes(prevCodes => {
+    if (enteredChar && /^\d$/.test(enteredChar)) {
+      setCodes((prevCodes) => {
         const newCodes = [...prevCodes];
         newCodes[index] = enteredChar;
         return newCodes;
       });
 
-      if (index < CODE_LENGTH - 1 && inputRefs.current[index + 1]){
+      if (index < CODE_LENGTH - 1 && inputRefs.current[index + 1]) {
         inputRefs.current[index + 1]?.focus();
       }
     }
-  }
+  };
   useEffect(() => {
     if (inputRefs.current[0]) {
       inputRefs.current[0].focus();
     }
-  }, []); 
-
+  }, []);
 
   return (
     <div className={styles['modal-container']}>
       <div className={styles['modal-content']}>
-        <div onClick={onClose} className={styles['modal-content__return-arrow']}>
+        <div
+          onClick={onClose}
+          className={styles['modal-content__return-arrow']}
+        >
           <img src={iconArray} alt="prev-page" />
         </div>
 
@@ -51,50 +71,22 @@ function RegisterModal({ onClose } : RegisterModalProps) {
         <div className={styles['modal-content__code-box']}>
           <h5 className={styles['code-box__title']}>Пожалуйста, введите код</h5>
           <ul className={styles['code-box__line-code']}>
-            <li className={styles['code-box__code-item']}>
-              <input 
-              type="text" 
-              maxLength="1" 
-              inputMode="numeric"
-              pattern="\d"/>
-            </li>
-                        <li className={styles['code-box__code-item']}>
-              <input 
-              type="text" 
-              maxLength="1" 
-              inputMode="numeric"
-              pattern="\d"/>
-            </li>
-                        <li className={styles['code-box__code-item']}>
-              <input 
-              type="text" 
-              maxLength="1" 
-              inputMode="numeric"
-              pattern="\d"/>
-            </li>
-                        <li className={styles['code-box__code-item']}>
-              <input 
-              type="text" 
-              maxLength="1" 
-              inputMode="numeric"
-              pattern="\d"/>
-            </li>
-                        <li className={styles['code-box__code-item']}>
-              <input 
-              type="text" 
-              maxLength="1" 
-              inputMode="numeric"
-              pattern="\d"/>
-            </li>
-                        <li className={styles['code-box__code-item']}>
-              <input 
-              type="text" 
-              maxLength="1" 
-              inputMode="numeric"
-              pattern="\d"/>
-            </li>
-
-
+            {Array(CODE_LENGTH)
+              .fill(null)
+              .map((_, index) => (
+                <li key={index} className={styles['code-box__code-item']}>
+                  <input
+                    type="text"
+                    maxLength={1}
+                    ref={(el) => (inputRefs.current[index] = el)}
+                    value={codes[index]}
+                    onChange={(e) => handleChange(e, index)}
+                    onKeyDown={(e) => handleKeyDown(e, index)}
+                    inputMode="numeric"
+                    pattern="\d"
+                  />
+                </li>
+              ))}
           </ul>
         </div>
 
