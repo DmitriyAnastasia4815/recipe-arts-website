7b66e17ca2eb99c8f09cb34d9b93c5a840a7a71c
Nastya <the.n_kocherovaaa@bk.ru>
2025-07-17 11:53:41 +0300

    Добавила документацию для компонента SuccessOperation

--- frontend/src/features/Auth/components/SuccessOperation/SuccessOperation.tsx
+++ frontend/src/features/Auth/components/SuccessOperation/SuccessOperation.tsx
@@ -1,29 +1,47 @@
+/**
+ * Компонент SuccessOperation отображает модальное окно с сообщением об успехе или ошибке
+ * @param {SuccessOperationProps} props - Свойства компонента
+ * @returns {JSX.Element} Модальное окно с состоянием успеха или ошибки
+ */
+
 import React from 'react';
-import { useEffect, useState,  } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useEffect, } from 'react';
+
 import styles from './SuccessOperation.module.scss';
 
 import rightIcon from '@icon/icon-right-password.svg';
 import wrongIcon from '@icon/icon-false-password.svg';
 
+
+/**
+ * Интерфейс свойств для компонента SuccessOperation
+ */
 interface SuccessOperationProps {
+  /** Указывает, была ли операция успешной */
   success: boolean,
+  /** Функция обратного вызова для закрытия модального окна */
   onClose: () => void,
 }
+
+
+
+/**
+ * Компонент SuccessOperation
+ * Отображает модальное окно, которое автоматически закрывается через 2 секунды
+ * @param props - Свойства компонента SuccessOperationProps
+ */
 
 export const SuccessOperation: React.FC<SuccessOperationProps> = ({
   success,
   onClose
 }) => {
 
-  const navigate = useNavigate();
-
   useEffect(() => {
     const timer = setTimeout(() => {
       onClose()
-    }, 5000);
+    }, 2000);
     return () => clearTimeout(timer);
-  }, []);
+  }, [onClose]);
 
 
   return (
@@ -31,12 +49,12 @@ export const SuccessOperation: React.FC<SuccessOperationProps> = ({
       <div className={styles['modal-content-success']}>
         {success ? (
           <div className={styles['modal-content-success__block']}>
-            <img src={rightIcon} alt="" />
+            <img src={rightIcon} alt="Успешно" />
             <h4>Пароль успешно изменен</h4>
           </div>
         ) : (
           <div className={styles['modal-content-success__block']}>
-            <img src={wrongIcon} alt="" />
+            <img src={wrongIcon} alt="Ошибка" />
             <h4>Произошла ошибка, попробуйте снова</h4>
           </div>
         )}
