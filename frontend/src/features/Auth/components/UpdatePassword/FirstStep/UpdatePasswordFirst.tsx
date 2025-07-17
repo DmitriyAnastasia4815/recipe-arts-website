07c859c0a3c65fa72c626250a2395e9fbc36c347
Nastya <the.n_kocherovaaa@bk.ru>
2025-07-17 13:13:13 +0300

    Добавила сообщение о невалидной почте в компоненте UpdatePAsssword First step

--- frontend/src/features/Auth/components/UpdatePassword/FirstStep/UpdatePasswordFirst.tsx
+++ frontend/src/features/Auth/components/UpdatePassword/FirstStep/UpdatePasswordFirst.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import styles from './UpdatePasswordFirst.module.scss';
 import iconArray from '@icon/icon-array.svg';
@@ -5,7 +6,6 @@ import iconArray from '@icon/icon-array.svg';
 import { validEmail } from '@/features/Auth/validation/validation';
 
 import { useState, useCallback, useRef, useEffect } from 'react';
-
 import { ChangeEvent } from 'react';
 
 const TEXT_STEP1 = {
@@ -26,14 +26,22 @@ export const UpdatePasswordFirst: React.FC<UpdatePasswordFirstProps> = ({
 }) => {
   const [email, setEmail] = useState('');
   const [emailError, setEmailError] = useState(false);
+  const [isBlurred, setIsBlurred] = useState(false);
 
   const inputRef = useRef<HTMLInputElement>(null);
 
   const checkEmailInput = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     setEmail(value);
-    setEmailError(!validEmail(value));
-  }, []);
+    if (isBlurred) {
+      setEmailError(!validEmail(value));
+    }
+  }, [isBlurred]);
+
+  const handleBlur = useCallback(() => {
+    setIsBlurred(true);
+    setEmailError(!validEmail(email));
+  }, [email]);
 
   useEffect(() => {
     inputRef.current?.focus();
@@ -49,11 +57,18 @@ export const UpdatePasswordFirst: React.FC<UpdatePasswordFirstProps> = ({
 
       <div className={styles['modal-content__code-box']}>
         <h5 className={styles['code-box__title']}>{TEXT_STEP1.SUBTITLE}</h5>
+        <h5
+          className={`${isBlurred && emailError ? styles['code-box__error-message--visible'] : styles['code-box__error-message']}`}
+        >
+          Почта указана неверно, проверьте еще раз
+        </h5>
+
         <input
-          className={`${styles['code-box__input']} ${emailError ? styles['code-box__input--error'] : ''}`}
+          className={`${styles['code-box__input']} ${isBlurred && emailError ? styles['code-box__input--error'] : ''}`}
           type="email"
           placeholder={TEXT_STEP1.PLACEHOLDER}
           onChange={checkEmailInput}
+          onBlur={handleBlur}
           ref={inputRef}
         />
       </div>
@@ -68,3 +83,4 @@ export const UpdatePasswordFirst: React.FC<UpdatePasswordFirstProps> = ({
     </>
   );
 };
+
